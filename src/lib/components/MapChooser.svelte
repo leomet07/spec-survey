0817ca9035b398463cdc05d7b560726f26f0ba1d
leomet07 <leomet07@gmail.com>
2023-09-18 17:48:03 -0400

    Style map chooser component

--- src/lib/components/MapChooser.svelte
+++ src/lib/components/MapChooser.svelte
@@ -9,16 +9,11 @@
     onMount(async () => {
         if(browser) {
             const leaflet = await import('leaflet');
-
-            map = leaflet.map(mapElement).setView([40.718139, -74.013754], 13);
+            map = leaflet.map(mapElement).setView([40.718139, -74.013754], 2);
 
             leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
-
-            leaflet.marker([40.718139, -74.013754]).addTo(map)
-                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-                .openPopup();
         }
     });
 
@@ -30,13 +25,34 @@
     });
 </script>
 
-<main>
-    <div bind:this={mapElement}></div>
-</main>
+<div class="map" bind:this={mapElement}></div>
 
 <style>
     @import 'leaflet/dist/leaflet.css';
-    main div {
-        height: 800px;
+    .map {
+        height: 600px;
     }
+
+    /* Override styles applied by pocketbase */
+    :global(.leaflet-control-zoom){
+        border : none !important;
+        padding-bottom: 2px !important;
+    }
+    :global(.leaflet-control-zoom [role="button"]){
+        padding : 0px !important; 
+        border-radius: 0% !important;;
+    }
+    :global(.leaflet-control-zoom a:last-child){
+        border-bottom : 1px solid var(--primary) !important
+    }
+
+    :global(.leaflet-marker-icon){
+        background-color: transparent !important;
+        border : none !important;
+    }
+
+    :global(.leaflet-popup-close-button){
+        padding : 0px !important;
+    }
+    
 </style>
