1f06ec3dce262ca35b5915623328f7affe790cb1
leomet07 <leomet07@gmail.com>
2023-09-21 00:46:16 -0400

    switch pocketbase uri

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,9 @@
 import PocketBase from "pocketbase";
 import { writable } from "svelte/store";
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
+import { dev } from '$app/environment';
 
-export const pb = new PocketBase("http://127.0.0.1:8090");
+export const pb = new PocketBase(dev ? "http://127.0.0.1:8090" : PUBLIC_POCKETBASE_URL);
 
 export const currentUser = writable(pb.authStore.model);
 
