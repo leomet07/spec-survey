ede9f6e8e6eb12639bb0708148629998119ef481
leomet07 <leomet07@gmail.com>
2023-09-20 21:07:41 -0400

    Get basic predictions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import MapChooser from "$lib/components/MapChooser.svelte";
 	import { pb, currentUser } from "$lib/pocketbase";
+	import { onMount } from 'svelte';
+	import { PUBLIC_GMAPS_JSAPI_KEY } from '$env/static/public';
 
 	async function loginWithGoogle(){
 		// This method initializes a one-off realtime subscription and will
@@ -16,7 +18,35 @@
 		// "logout" the last authenticated model
 		pb.authStore.clear();
 	}
+
+
+	onMount(() => {
+		initService();
+	});
+
+	function initService(): void {
+		const displaySuggestions = function (
+			predictions: google.maps.places.QueryAutocompletePrediction[] | null,
+			status: google.maps.places.PlacesServiceStatus
+		) {
+			if (status != google.maps.places.PlacesServiceStatus.OK || !predictions) {
+			alert(status);
+			return;
+			}
+
+			console.log("Predictions: ", predictions)
+		};
+
+		const service = new google.maps.places.AutocompleteService();
+
+		service.getQueryPredictions({ input: "pizza near Syd" }, displaySuggestions);
+	}
+	
 </script>
+
+<svelte:head>
+	<script src={`https://maps.googleapis.com/maps/api/js?key=${PUBLIC_GMAPS_JSAPI_KEY}&libraries=places`} async defer/>
+</svelte:head>
 
 <h1>Hello World</h1>
 <button on:click|preventDefault={loginWithGoogle}>Login with Google</button>
