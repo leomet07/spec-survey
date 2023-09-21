1700c2a16a503a05ac1915d039fd1a42f522997a
leomet07 <leomet07@gmail.com>
2023-09-21 00:14:28 -0400

    make question updater reusable

--- src/lib/components/Questionare.svelte
+++ src/lib/components/Questionare.svelte
@@ -3,20 +3,19 @@
 	import { q1_results, q2_results } from "$lib/store";
 	import QuestionComponent from "$lib/components/QuestionComponent.svelte";
 	import { ClientResponseError } from "pocketbase";
+	import type { QuestionResults } from "$lib/types";
 
 	export let autoCompleteService: google.maps.places.AutocompleteService;
 	export let geocoder: google.maps.Geocoder;
 
-	q1_results.subscribe(async (q1) => {
-		if (!$currentUser || !q1) {
+    async function questionUpdater(questionStore : QuestionResults | undefined, question_id : string){
+        if (!$currentUser || !questionStore) {
 			return;
 		}
-		console.log("Q1 change: ");
-		console.log(q1);
 
         let found;
         try {
-            found = await pb.collection("questions").getFirstListItem(`question_id="A"`);
+            found = await pb.collection("questions").getFirstListItem(`question_id="${question_id}"`);
         } catch (e: any){
             if (e.status != 404 && !(e instanceof ClientResponseError) ){ // if 404, just return false
                 throw e;
@@ -24,19 +23,21 @@
         }
 
         const data = {
-            lat : q1.latlng.lat,
-            lng : q1.latlng.lat,
-            political_address_components : q1.political_address_components,
+            lat : questionStore.latlng.lat,
+            lng : questionStore.latlng.lat,
+            political_address_components : questionStore.political_address_components,
             parent_user : $currentUser.id,
-            question_id : "A"
+            question_id : question_id
         }
         
         if (found){
             const deleted = await pb.collection("questions").delete(found.id);
         }
         const created = await pb.collection("questions").create(data);
+    }
 
-        console.log("Does exist? ", !!found);
+	q1_results.subscribe(async (q1) => {
+		questionUpdater(q1, "A");
 	});
 </script>
 
