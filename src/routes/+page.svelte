a9b86c8cf2542fe936e3c3fd910a1f839107f596
leomet07 <leomet07@gmail.com>
2023-09-21 02:48:46 -0400

    JANKY WORKAROUND FOR AUTH ON SAFARI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
 	import { pb, currentUser } from "$lib/pocketbase";
 	import { onMount } from 'svelte';
 
+	let errord : any;
+	let url : string;
 	async function loginWithGoogle(){
 		// This method initializes a one-off realtime subscription and will
 		// open a popup window with the OAuth2 vendor page to authenticate.
@@ -10,7 +12,18 @@
 		// Once the external OAuth2 sign-in/sign-up flow is completed, the popup
 		// window will be automatically closed and the OAuth2 data sent back
 		// to the user through the previously established realtime connection.
-		const authData = await pb.collection('users').authWithOAuth2({ provider: 'google' });
+		try {
+			errord = "before"
+			const authData = await pb.collection('users').authWithOAuth2({ provider: 'google', urlCallback : (urlp : string) => {
+				url = urlp;
+				// window.location.href = urlp;
+				window.open(urlp); // just try and open the window right away, works on non IOS browsers
+			}});
+			errord = "after"
+		} catch (error){
+			errord = "before"
+			errord = String(error);
+		}
 	}
 
 	async function logout(){
@@ -19,8 +32,11 @@
 	}
 
 
-	onMount(() => {
+	onMount(async () => {
 		initService();
+		const authData = await pb.collection('users').authWithOAuth2({ provider: 'google', urlCallback : (urlp : string) => {
+			url = urlp;
+		}});
 	});
 	let autoCompleteService : google.maps.places.AutocompleteService;
 	let geocoder : google.maps.Geocoder;
@@ -40,7 +56,11 @@
 	<button on:click|preventDefault={logout} class="secondary outline">Logout</button>
 {:else}
 	<p>You're not logged in</p>
-	<button on:click|preventDefault={loginWithGoogle}>Login with Google</button>
+	<!-- <button on:click|preventDefault={loginWithGoogle}>Login with Google</button> -->
+	<details>
+		<summary role="button" on:click={loginWithGoogle} on:keypress={loginWithGoogle} tabindex="1">Sign in</summary>
+		<a role="button" target="_blank" href={url}>New login with google</a>
+	</details>
 {/if}
 
 <style>
