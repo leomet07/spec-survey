9f08483fa96ecd1e4687d8139770784d681e744b
leomet07 <leomet07@gmail.com>
2023-10-09 21:26:16 -0400

    use a question mapping and load questions from db

--- src/lib/components/Questionare.svelte
+++ src/lib/components/Questionare.svelte
@@ -2,7 +2,7 @@
 	import { pb, currentUser } from "$lib/pocketbase";
 	import * as store from "$lib/store";
 	import QuestionComponent from "$lib/components/QuestionComponent.svelte";
-	import { ClientResponseError } from "pocketbase";
+	import { ClientResponseError, type RecordModel } from "pocketbase";
 	import type { QuestionResults } from "$lib/types";
 
 	export let autoCompleteService: google.maps.places.AutocompleteService;
@@ -13,6 +13,20 @@
 		question_id: string
 	) {
 		if (!$currentUser || !questionStore) {
+			return;
+		}
+
+		const matched_to_on_load = found_answers?.filter(
+			(v: any) => v.question_id == question_id
+		);
+
+		if (
+			matched_to_on_load.length > 0 &&
+			matched_to_on_load[0]?.lat == questionStore.latlng.lat &&
+			matched_to_on_load[0]?.lng == questionStore.latlng.lng
+		) {
+			console.log("Returning early, this is from the initial on load");
+			// nothing has changed from the initial markers from db, do not do any db stuff
 			return;
 		}
 
@@ -38,53 +52,69 @@
 		};
 
 		if (found) {
+			if (found.lat == data.lat && found.lng == data.lng) {
+				// nothing has changed, do not do any db stuff
+				return;
+			}
 			const deleted = await pb.collection("questions").delete(found.id);
 		}
 		const created = await pb.collection("questions").create(data);
 	}
 
-	store.q1_results.subscribe(async (q1) => {
-		questionUpdater(q1, "A");
-	});
-	store.q2_results.subscribe(async (q2) => {
-		questionUpdater(q2, "B");
-	});
-	store.q3_results.subscribe(async (q3) => {
-		questionUpdater(q3, "C");
-	});
-	store.q4_results.subscribe(async (q4) => {
-		questionUpdater(q4, "D");
-	});
-	store.q5_results.subscribe(async (q5) => {
-		questionUpdater(q5, "E");
-	});
-	store.q6_results.subscribe(async (q6) => {
-		questionUpdater(q6, "F");
-	});
-	store.q7_results.subscribe(async (q7) => {
-		questionUpdater(q7, "G");
-	});
+	const question_mapping = {
+		q1_results: "A",
+		q2_results: "B",
+		q3_results: "C",
+		q4_results: "D",
+		q5_results: "E",
+		q6_results: "F",
+		q7_results: "G",
+	};
+
+	type question_keys = keyof typeof question_mapping;
+	async function listen_for_updates() {
+		for (const key in question_mapping) {
+			store[key as question_keys].subscribe(async (q) => {
+				questionUpdater(q, question_mapping[key as question_keys]);
+			});
+		}
+	}
 
 	currentUser.subscribe(async (curr_user) => {
-		if (curr_user?.id){
-			load_questions()
+		if (curr_user?.id) {
+			load_questions();
 		}
-	})
+	});
 
-	async function load_questions(){
-		let found_answers = await pb.collection("questions").getFullList();
-			for (const found_answer of found_answers) {
-				if (found_answer.question_id == "A") {
-					store['q1_results'].set({
-						latlng: {
-							lat: found_answer.lat,
-							lng: found_answer.lng,
-						},
-						political_address_components:
-							found_answer.political_address_components,
-					});
-				}
+	let found_answers: any | undefined;
+
+	async function load_questions() {
+		found_answers = await pb
+			.collection("questions")
+			.getFullList({ requestKey: "load_questions" });
+		console.log("Found answers: ", found_answers);
+
+		for (const found_answer of found_answers) {
+			let found_entry_in_store = Object.entries(question_mapping).filter(
+				(a) => a[1] == found_answer.question_id
+			);
+			if (found_entry_in_store.length == 0) {
+				throw new Error(
+					"No store was found for database question_id of: " +
+						found_answer.question_id
+				);
 			}
+			store[found_entry_in_store[0][0] as question_keys].set({
+				latlng: {
+					lat: found_answer.lat,
+					lng: found_answer.lng,
+				},
+				political_address_components:
+					found_answer.political_address_components,
+			});
+		}
+
+		listen_for_updates();
 	}
 </script>
 
