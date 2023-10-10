6576184dc5b2675eb03d01bf8e8eb704eb1b5ef5
leomet07 <leomet07@gmail.com>
2023-10-09 20:50:14 -0400

    lint + basic question loading

--- src/lib/components/Questionare.svelte
+++ src/lib/components/Questionare.svelte
@@ -8,33 +8,40 @@
 	export let autoCompleteService: google.maps.places.AutocompleteService;
 	export let geocoder: google.maps.Geocoder;
 
-    async function questionUpdater(questionStore : QuestionResults | undefined, question_id : string){
-        if (!$currentUser || !questionStore) {
+	async function questionUpdater(
+		questionStore: QuestionResults | undefined,
+		question_id: string
+	) {
+		if (!$currentUser || !questionStore) {
 			return;
 		}
 
-        let found;
-        try {
-            found = await pb.collection("questions").getFirstListItem(`question_id="${question_id}"`);
-        } catch (e: any){
-            if (e.status != 404 && !(e instanceof ClientResponseError) ){ // if 404, just return false
-                throw e;
-            }
-        }
+		let found;
+		try {
+			found = await pb
+				.collection("questions")
+				.getFirstListItem(`question_id="${question_id}"`);
+		} catch (e: any) {
+			if (e.status != 404 && !(e instanceof ClientResponseError)) {
+				// if 404, just return false
+				throw e;
+			}
+		}
 
-        const data = {
-            lat : questionStore.latlng.lat,
-            lng : questionStore.latlng.lng,
-            political_address_components : questionStore.political_address_components,
-            parent_user : $currentUser.id,
-            question_id : question_id
-        }
-        
-        if (found){
-            const deleted = await pb.collection("questions").delete(found.id);
-        }
-        const created = await pb.collection("questions").create(data);
-    }
+		const data = {
+			lat: questionStore.latlng.lat,
+			lng: questionStore.latlng.lng,
+			political_address_components:
+				questionStore.political_address_components,
+			parent_user: $currentUser.id,
+			question_id: question_id,
+		};
+
+		if (found) {
+			const deleted = await pb.collection("questions").delete(found.id);
+		}
+		const created = await pb.collection("questions").create(data);
+	}
 
 	store.q1_results.subscribe(async (q1) => {
 		questionUpdater(q1, "A");
@@ -57,6 +64,28 @@
 	store.q7_results.subscribe(async (q7) => {
 		questionUpdater(q7, "G");
 	});
+
+	currentUser.subscribe(async (curr_user) => {
+		if (curr_user?.id){
+			load_questions()
+		}
+	})
+
+	async function load_questions(){
+		let found_answers = await pb.collection("questions").getFullList();
+			for (const found_answer of found_answers) {
+				if (found_answer.question_id == "A") {
+					store['q1_results'].set({
+						latlng: {
+							lat: found_answer.lat,
+							lng: found_answer.lng,
+						},
+						political_address_components:
+							found_answer.political_address_components,
+					});
+				}
+			}
+	}
 </script>
 
 <QuestionComponent
