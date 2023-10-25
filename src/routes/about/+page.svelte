023fe21f3a17df0380e3de08b420a172beccc8e4
leomet07 <leomet07@gmail.com>
2023-10-24 20:32:12 -0400

    Add basic about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+    <title>About - The Spectator Demographics Survey</title>
+</svelte:head>
+
+<main>
+    <article>
+        <h1>About The Spectator Demographics Survey</h1>
+        <section>
+            <h2>Privacy</h2>
+            <p>
+                This survey is optional, but highly encouraged, for all
+                Stuyvesant students.
+            </p>
+            <p>
+                Some question response data is only revealed to the public
+                anonymously in bulk with other students. All other information
+                is private.
+            </p>
+        </section>
+        <section>
+            <h2>This website is open source</h2>
+            <p>
+                We aren't lying, but if anybody doubts the validity of this
+                site, it's source code is free and open source. <a
+                    href="https://github.com/leomet07/garfinkel_survey"
+                    >It is available on github.</a
+                >
+            </p>
+        </section>
+    </article>
+</main>
