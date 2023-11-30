10e1607443b1a5fd44153b807389a80fda63f135
leomet07 <leomet07@gmail.com>
2023-11-29 23:21:07 -0500

    Update about page copy

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,31 +1,42 @@
 <svelte:head>
-    <title>About - The Spectator Demographics Survey</title>
+	<title>About - The Spectator Demographics Survey</title>
 </svelte:head>
 
 <main>
-    <article>
-        <h1>About The Spectator Demographics Survey</h1>
-        <section>
-            <h2>Privacy</h2>
-            <p>
-                This survey is optional, but highly encouraged, for all
-                Stuyvesant students.
-            </p>
-            <p>
-                Some question response data is only revealed to the public
-                anonymously in bulk with other students. All other information
-                is private.
-            </p>
-        </section>
-        <section>
-            <h2>This website is open source</h2>
-            <p>
-                We aren't lying, but if anybody doubts the validity of this
-                site, it's source code is free and open source. <a
-                    href="https://github.com/leomet07/garfinkel_survey"
-                    >It is available on github.</a
-                >
-            </p>
-        </section>
-    </article>
+	<article>
+		<h1>About The Spectator Demographics Survey</h1>
+		<p>
+			This survey aims to collect far more detailed information about the
+			demographics of the Stuyvesant student body and community than any
+			other survey has, including the yearly incoming Freshman survey by
+			The Spectator and the NYCDOE's census.
+		</p>
+		<section>
+			<h2>Privacy</h2>
+			<p>
+				This survey is optional, but highly encouraged, for all
+				Stuyvesant students.
+			</p>
+			<p>
+				Ancestry question response data is only revealed to the public
+				anonymously in bulk with other students. All other information
+				is private.
+			</p>
+		</section>
+		<section>
+			<h2>This website is open source</h2>
+			<p>
+				This website is free and open source - it's source code is free
+				and open source. <a
+					href="https://github.com/leomet07/spec-survey"
+					>It is available on GitHub.</a
+				>
+			</p>
+		</section>
+		<footer>
+			Built with &lt;3 by <a href="https://github.com/leomet07"
+				>Lenny Metlitsky ('25)</a
+			>, a Spectator Web editor
+		</footer>
+	</article>
 </main>
