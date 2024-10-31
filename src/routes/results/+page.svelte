51018b76e92120b096c1277d071c09d1335b76f4
leomet07 <leomet07@gmail.com>
2024-10-31 12:36:47 -0400

    basic chart with simple colors

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -2,17 +2,33 @@
 	import { pb } from "$lib/pocketbase";
 	import { onMount } from "svelte";
 
-	import type { Map } from "leaflet";
+	// import { Map } from "leaflet";
 	import { browser } from "$app/environment";
 
 	let mapElement: HTMLElement;
-	let map: Map;
+	let chartElement: HTMLElement;
+	let map: any;
+
+	let names: string[] = [];
+	let values: number[] = [];
+	import { Pie } from "svelte-chartjs";
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		ArcElement,
+		CategoryScale,
+		Colors,
+	} from "chart.js";
+
+	ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale, Colors);
 
 	onMount(async () => {
 		// pb.collection("public_questions").subscribe("*" , (public_answers) => {
 		//     console.log("Public answers: ", public_answers)
 		// })
-		if (browser) {
+		if (browser && window) {
 			const leaflet = await import("leaflet");
 
 			map = leaflet
@@ -48,6 +64,59 @@
 						console.log("Marker clicked: ", public_answer);
 					});
 			}
+			const only_question_a = public_answers.filter(
+				(v) => v.question_id.toLowerCase() == "a",
+			);
+			const all_political_address_components = only_question_a.map(
+				(v) => v.political_address_components,
+			);
+
+			// const only_countries = all_political_address_components.map((v) => v.filter(j => j.types.includes("country")));
+			const only_countries = [];
+			for (
+				let index = 0;
+				index < all_political_address_components.length;
+				index++
+			) {
+				let political_address_component =
+					all_political_address_components[index];
+				let v = political_address_component.filter((j: any) =>
+					j.types.includes("country"),
+				);
+
+				if (v.length == 0) {
+					// no political address components
+					continue;
+				}
+
+				only_countries.push(v);
+			}
+			const only_country_names = only_countries.map(
+				(v) => v[0]?.short_name,
+			);
+
+			let country_name_to_number_map: Record<string, number> = {};
+
+			for (const country_name of only_country_names) {
+				if (country_name in country_name_to_number_map) {
+					country_name_to_number_map[country_name] += 1;
+				} else {
+					country_name_to_number_map[country_name] = 1;
+				}
+			}
+			const object_entry_map = Object.entries(country_name_to_number_map);
+			object_entry_map.sort((a, b) => b[1] - a[1]);
+			console.log(object_entry_map);
+
+			names = object_entry_map.map((a) => a[0]);
+			values = object_entry_map.map((a) => a[1]);
+
+			// console.log("192: ", only_country_names[192]);
+
+			// const unique_set_of_country_names = Array.from(
+			// 	new Set(only_country_names),
+			// );
+			// console.log("Unique country names: ", unique_set_of_country_names);
 		}
 	});
 </script>
@@ -55,6 +124,18 @@
 <h1>Results</h1>
 
 <div class="map" bind:this={mapElement} />
+
+<Pie
+	data={{
+		labels: names,
+		datasets: [
+			{
+				data: values,
+			},
+		],
+	}}
+	options={{ responsive: true }}
+/>
 
 <style>
 	@import "leaflet/dist/leaflet.css";
